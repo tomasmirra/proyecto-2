8a340717ac618163e03923f0e8a0e8b1e5ed4ca1
Javier Martinez Ribo <javiermribo@gmail.com>
2021-06-24 14:04:57 -0300

    tarea añadir cancion.

---
+++ js/pagina-admin.js
@@ -0,0 +1,70 @@
+class Songs {
+  constructor(title, duration, album, artist, category) {
+    this.id = Date.now();
+    this.title = title;
+    this.duration = duration;
+    this.album = album;
+    this.artist = artist;
+    this.category = category;
+  }
+}
+
+class UI {
+  static displaySongs() {
+    const storedSongs = Store.getSongs();
+    storedSongs.forEach((song) => UI.addSongsToList(song)); //Recorre el array de canciones y las muestra en la pagina
+  }
+
+  static addSongsToList(song) {
+    const SONGLIST_SELECTOR = document.querySelector("#songList");
+    const row = document.createElement("tr");
+    row.id = song.id;
+    row.innerHTML = `
+    <td>${song.id}</td>
+    <td>${song.title}</td>
+    <td>${song.duration}</td>
+    <td>${song.album}</td>
+    <td>${song.artist}</td>
+    <td>${song.category}</td>
+    `;
+    SONGLIST_SELECTOR.append(row);
+    //Añadir después de <td>${song.category}</td> botones para eliminar, destacar y editar canción
+  }
+}
+
+class Store {
+  static getSongs() {
+    //Método estatico para que const storedSongs = Store.getSongs pueda acceder
+    let songs;
+    if (localStorage.getItem("songs") === null) {
+      songs = [];
+    } else {
+      songs = JSON.parse(localStorage.getItem("songs"));
+    }
+    return songs;
+  }
+
+  static addSongToLS(song) {
+    const songs = Store.getSongs();
+    songs.push(song);
+    localStorage.setItem("songs", JSON.stringify(songs));
+  }
+}
+
+  document.addEventListener("DOMContentLoaded", UI.displaySongs);
+
+const FORM_SELECTOR = document.querySelector("#song-form");
+FORM_SELECTOR.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = document.querySelector("#songTtile").value;
+  const duration = document.querySelector("#songDuration").value;
+  const album = document.querySelector("#songAlbum").value;
+  const artist = document.querySelector("#songArtist").value;
+  const category = document.querySelector("#songCategory").value;
+
+  const song = new Songs(title, duration, album, artist, category);
+
+  UI.addSongsToList(song);
+
+  Store.addSongToLS(song);
+});
